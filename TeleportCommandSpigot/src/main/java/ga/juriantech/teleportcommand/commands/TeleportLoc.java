a815aae3de1eea4bb2103d45eeb06d471201816e
jurian112211 <87150143+jurian112211@users.noreply.github.com>
2022-05-28 22:04:05 +0200

    More colors

--- TeleportCommandSpigot/src/main/java/ga/juriantech/teleportcommand/commands/TeleportLoc.java
+++ TeleportCommandSpigot/src/main/java/ga/juriantech/teleportcommand/commands/TeleportLoc.java
@@ -20,7 +20,7 @@ public class TeleportLoc extends BaseCommand {
             Location location = new Location(player.getWorld(), plugin.getConfig().getInt("location.x"), plugin.getConfig().getInt("location.y"), plugin.getConfig().getInt("location.z"));
             player.teleport(location);
             // TODO, add a check if it succeeded.
-            player.sendMessage(plugin.getConfig().getString("messages.teleported").replace("{cords}", (CharSequence) player.getLocation()));
+            player.sendMessage(ChatColor.GREEN + plugin.getConfig().getString("messages.teleported").replace("{cords}", (CharSequence) player.getLocation()));
 
         }
     }
