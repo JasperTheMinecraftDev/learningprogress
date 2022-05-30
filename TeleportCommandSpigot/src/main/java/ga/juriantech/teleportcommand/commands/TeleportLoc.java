c364e9939d431f9f862b1037bd745eb6b5b6a5bd
jurian112211 <87150143+jurian112211@users.noreply.github.com>
2022-05-30 15:43:31 +0200

    Solve the errors & list teleported location.

--- TeleportCommandSpigot/src/main/java/ga/juriantech/teleportcommand/commands/TeleportLoc.java
+++ TeleportCommandSpigot/src/main/java/ga/juriantech/teleportcommand/commands/TeleportLoc.java
@@ -21,7 +21,7 @@ public class TeleportLoc extends BaseCommand {
             player.sendMessage(ChatColor.YELLOW + plugin.getConfig().getString("messages.teleporting"));
             Location location = new Location(player.getWorld(), plugin.getConfig().getInt("location.x"), plugin.getConfig().getInt("location.y"), plugin.getConfig().getInt("location.z"));
             player.teleport(location);
-            player.sendMessage(ChatColor.GREEN + plugin.getConfig().getString("messages.teleported").replace("{cords}", (CharSequence) player.getLocation()));
+            player.sendMessage(ChatColor.GREEN + plugin.getConfig().getString("messages.teleported").replace("{cords}",  location.getX() + " " + location.getY() + " " + location.getZ()));
 
         }
     }
