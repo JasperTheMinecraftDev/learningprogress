64fa05966fa7cb457b3dc4b0585ff457176ad94f
jurian112211 <87150143+jurian112211@users.noreply.github.com>
2022-06-01 17:53:19 +0200

    Upload GameModeCommand!

---
+++ GamemodeCommandSpigot/src/main/java/ga/juriantech/gamemodecommandspigot/GamemodeCommandSpigot.java
@@ -0,0 +1,43 @@
+package ga.juriantech.gamemodecommandspigot;
+
+import co.aikar.commands.BukkitCommandManager;
+import com.google.common.collect.ImmutableList;
+import ga.juriantech.gamemodecommandspigot.commands.GameMode;
+import org.bukkit.Bukkit;
+import org.bukkit.plugin.java.JavaPlugin;
+
+import java.util.logging.Logger;
+
+public final class GamemodeCommandSpigot extends JavaPlugin {
+
+    public Logger logger = Bukkit.getLogger();
+    GamemodeCommandSpigot plugin;
+    private static GamemodeCommandSpigot instance;
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+        instance = this;
+        this.plugin = getInstance();
+        commands();
+        config();
+        logger.warning("GamemodeCommand has been started!");
+
+    }
+    private void config() {
+        plugin.saveDefaultConfig();
+    }
+
+    private void commands() {
+        BukkitCommandManager manager = new BukkitCommandManager(plugin);
+        manager.registerCommand(new GameMode());
+    }
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+        logger.warning("GamemodeCommand has stopped.");
+    }
+
+    public static GamemodeCommandSpigot getInstance() {
+        return instance;
+    }
+}
---
+++ GamemodeCommandSpigot/src/main/java/ga/juriantech/gamemodecommandspigot/commands/GameMode.java
@@ -0,0 +1,57 @@
+package ga.juriantech.gamemodecommandspigot.commands;
+
+import co.aikar.commands.BaseCommand;
+import co.aikar.commands.annotation.CommandAlias;
+import co.aikar.commands.annotation.CommandCompletion;
+import co.aikar.commands.annotation.Default;
+import co.aikar.commands.annotation.Subcommand;
+import ga.juriantech.gamemodecommandspigot.GamemodeCommandSpigot;
+import ga.juriantech.gamemodecommandspigot.Utils.ChatUtils;
+import org.bukkit.entity.Player;
+
+@CommandAlias("gamemodes")
+public class GameMode extends BaseCommand {
+
+    GamemodeCommandSpigot plugin;
+
+    @Subcommand("creative")
+    @CommandCompletion("creative")
+    public void creative(Player player) {
+        this.plugin = GamemodeCommandSpigot.getInstance();
+        checkperm(player);
+        player.setGameMode(org.bukkit.GameMode.CREATIVE);
+        player.sendMessage(ChatUtils.colorize(plugin.getConfig().getString("messages.gamemode-changed").replace("{gamemode}", "Creative")));
+    }
+
+    @Subcommand("survival")
+    @CommandCompletion("survival")
+    public void survival(Player player) {
+        checkperm(player);
+        player.setGameMode(org.bukkit.GameMode.SURVIVAL);
+        player.sendMessage(ChatUtils.colorize(plugin.getConfig().getString("messages.gamemode-changed").replace("{gamemode}", "Survival")));
+
+    }
+
+    @Subcommand("spectator")
+    @CommandCompletion("spectator")
+    public void spectator(Player player) {
+        checkperm(player);
+        player.setGameMode(org.bukkit.GameMode.SPECTATOR);
+        player.sendMessage(ChatUtils.colorize(plugin.getConfig().getString("messages.gamemode-changed").replace("{gamemode}", "Spectator")));
+
+    }
+
+    @Subcommand("adventure")
+    @CommandCompletion("adventure")
+    public void adventure(Player player) {
+        checkperm(player);
+        player.setGameMode(org.bukkit.GameMode.ADVENTURE);
+        player.sendMessage(ChatUtils.colorize(plugin.getConfig().getString("messages.gamemode-changed").replace("{gamemode}", "Adventure")));
+
+    }
+
+    public void checkperm (Player player) {
+        if (!player.hasPermission("gamemodes.use"))
+            player.sendMessage(ChatUtils.colorize(plugin.getConfig().getString("messages.no-permission")));
+    }
+}
